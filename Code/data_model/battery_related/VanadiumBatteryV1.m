9b57892c59da83e14bf164cfb9fd5b2bec94398c
AitorIsp <147614901+AitorIsp@users.noreply.github.com>
2024-10-03 10:22:22 +0200

    Added electrolyte tank and vanadium specie

--- Code/data_model/battery_related/VanadiumBatteryV1.m
+++ Code/data_model/battery_related/VanadiumBatteryV1.m
@@ -1,23 +1,33 @@
-classdef VanadiumBatteryV1
-    %VANADIUMBATTERYV1 Summary of this class goes here
-    %   Detailed explanation goes here
-    
-    properties
-        Property1
+classdef VanadiumBatteryV1 < handle
+    % VanadiumBatteryV1 - first version of the vanadium battery representation
+    % 
+    %
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+    properties (Access=private)
+        
     end
-    
-    methods
-        function obj = VanadiumBatteryV1(inputArg1,inputArg2)
-            %VANADIUMBATTERYV1 Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    methods (Access=public)
+
+        %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Constructor %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %%
+        % -------------------------------------------------------------------------------------------------------------%
+        % -------------------------------------------------------------------------------------------------------------%
+
+        function obj = VanadiumBatteryV1()
+
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
+
     end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 end
 
