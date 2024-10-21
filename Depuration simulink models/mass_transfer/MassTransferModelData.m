cdd194f10c4c5392cdcce3544aa5bc4a360767ee
AitorIsp <147614901+AitorIsp@users.noreply.github.com>
2024-10-21 13:41:46 +0200

    Commit nuevo diagrama de objetos

--- Depuration simulink models/mass_transfer/MassTransferModelData.m
+++ Depuration simulink models/mass_transfer/MassTransferModelData.m
@@ -1,16 +1,16 @@
-clc
-clear all
-close all
+% clc
+% clear all
+% close all
 
 NCells = 120;
 
 VCell = 0.4968;
 VTank = 500;
 
-C2Ini = 0;
-C3Ini = 1.6;
-C4Ini = 1.6;
-C5Ini = 0;
+C2Ini = 1.6;
+C3Ini = 0;
+C4Ini = 0;
+C5Ini = 1.6;
 
 F = 96485;
 z = 1;
